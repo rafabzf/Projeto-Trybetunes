5713814082b29a0bc0cfc159951719ea10156a11
Rafael <rafinhabzf@gmail.com>
2022-08-02 20:29:43 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,80 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loadingMessage: false,
+      disabled: true,
+      userName: '',
+    };
+  }
+
   render() {
+    const maxLength = 3;
+
+    const {
+      loadingMessage,
+      disabled,
+      userName,
+    } = this.state;
+
+    const { history } = this.props;
+
+    const handleClickButton = async (e) => {
+      e.preventDefault();
+      this.setState({ loadingMessage: true });
+      await createUser({ name: userName });
+      this.setState({ loadingMessage: false });
+      history.push('/search');
+    };
+
+    const handleChange = ({ target }) => {
+      if (target.value.length < maxLength) {
+        this.setState({ disabled: true });
+      } else {
+        this.setState({ disabled: false });
+      }
+      this.setState({ userName: target.value });
+    };
+
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="login-name">
+            <input
+              type="text"
+              data-testid="login-name-input"
+              placeholder="Nome"
+              onChange={ handleChange }
+              value={ userName }
+            />
+          </label>
+          <br />
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ disabled }
+            onClick={ handleClickButton }
+          >
+            Entrar
+          </button>
+        </form>
+        {
+          loadingMessage && <Loading />
+        }
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  disabled: PropTypes.bool,
+  userName: PropTypes.string,
+  handleChange: PropTypes.func,
+}.isRequired;
 
 export default Login;
