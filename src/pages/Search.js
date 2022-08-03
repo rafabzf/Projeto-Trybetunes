590af6a96397fbfb247f1733e7570701d4a2f082
Rafael <rafinhabzf@gmail.com>
2022-08-03 18:46:32 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../components/Header';
 
-class Search extends React.Component {
+const minText = 2;
+
+class Search extends Component {
+  state = {
+    disabled: true,
+  }
+
+  onButtonInput = ({ target }) => {
+    if (target.value.length >= minText) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
+  }
+
   render() {
+    const { disabled } = this.state;
+
     return (
       <div data-testid="page-search">
         Profile Edit
         <Header />
+        <label htmlFor="search">
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            name="search"
+            onChange={ this.onButtonInput }
+          />
+        </label>
+
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ disabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
